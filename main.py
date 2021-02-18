e50408ea7a06a9ea3167a859eec7402361964df7
HM <hieumdd@gmail.com>
2021-02-18 14:27:09 +0700

    Added requirements.txt, modified main.yaml, added return for functions

--- main.py
+++ main.py
@@ -4,31 +4,35 @@ import hashlib
 import requests
 from google.cloud import storage
 
+
 class PageSpeedInsights:
     def __init__(self):
         self.api_key = os.environ.get("API_KEY")
-        self.bucket = storage.Client().bucket(os.environ.get('bucket'))
-        self.path_to_file = os.environ.get('PATH_TO_FILE')
-        
+        self.bucket = storage.Client().bucket(os.environ.get("bucket"))
+        self.path_to_file = os.environ.get("PATH_TO_FILE")
+
     def hash(self, data):
         hash_obj = hashlib.sha1(data.encode())
         return hash_obj.hexdigest()
 
-    def test(self):
+    def run(self):
         params = {
-            'key': self.api_key,
-            'url': 'https://vuanem.com',
-            'strategy': 'mobile'
+            "key": self.api_key,
+            "url": "https://vuanem.com",
+            "strategy": "mobile",
         }
         with requests.get(
             "https://www.googleapis.com/pagespeedonline/v5/runPagespeed", params=params
         ) as r:
-            blob = self.bucket.blob(self.path_to_file + hashlib.sha1(json.dumps(r.json()).encode()).hexdigest() + '.json')
-            blob.upload_from_string(
-                json.dumps(r.json())
-            )          
+            blob = self.bucket.blob(
+                self.path_to_file
+                + hashlib.sha1(json.dumps(r.json()).encode()).hexdigest()
+                + ".json"
+            )
+            blob.upload_from_string(json.dumps(r.json()))
 
 
-if __name__ == "__main__":
+def main(request):
     job = PageSpeedInsights()
-    job.test()
+    job.run()
+    return "ok"
